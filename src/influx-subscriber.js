1c586d556fd3680842dce5fd2aca05ce4fdc18d2
ot4kon <hubgrt.hubgrt@gmail.com>
2019-05-20 14:03:50 +0200

    Added Influx subscriber client implementation
    - influx subscriber implementation added
    - body-parser removed from dependencies
    - axios added to dependencies

--- src/influx-subscriber.js
+++ src/influx-subscriber.js
@@ -1,55 +1,173 @@
-const request = require("request");
+const axios = require("axios");
 
-class InfluxSubscriber {
-  constructor(opts) {
-    this.proto = opts.proto || "http";
-    this.host = opts.url || "localhost";
-    this.port = opts.port || 8086;
-    this.url = this.getUrl(this.proto, this.host, this.port);
-    this.db = opts.db || "_default";
-    this.username = opts.user || null;
-    this.password = opts.password || null;
-  }
 
-  getUrl(proto, host, port) {
-    return `${proto}://${host}:${port}/query`;
-  }
+// helper functions
 
-  getDatabases() {
-    // TODO
-  }
+const getApi = baseURL => {
+  return axios.create({
+    baseURL,
+    timeout: 5000,
+    headers: {}
+  });
+};
 
-  _getDestination(destinations) {
-    return destinations.map(dst => `'${dst}'`).join(",");
-  }
+const getUrl = (proto, host, port) => {
+  return `${proto}://${host}:${port}`;
+};
 
-  _getSubscriptionQuery({name, db, rp="autogen", all=true, destination} = {}) {
-    return `CREATE SUBSCRIPTION "${name}" ON "${db}"."${rp}" DESTINATIONS ${all ? "ALL" : "ANY"} ${this._getDestination(destination)}`;
-  }
+const getDestinations = destinations => destinations.map(dst => `'${dst}'`).join(",");
 
-  createSubscription(opts) {
-    const query = this._getSubscriptionQuery(opts);
 
-    request.post({
-      url: this.url,
-      form: {
-        db: this.db,
-        q: query,
-        u: this.username,
-        p: this.password
-      }
-    }, (err, res, body) => {
-      if (err) {
-        return console.error("Cannot create subscription!");
-      }
+// query literals
 
-      console.log(`Subscription on ${opts.db}.${opts.rp} created\n`, body);
-    });
-  }
+const QUERY_LITERALS = {
+  GET_DATABASES: () => "SHOW DATABASES;",
+  CREATE_SUBSCRIPTION: ({ name, db, rp, all, destination }) => `CREATE SUBSCRIPTION "${name}" ON "${db}"."${rp}" DESTINATIONS ${all ? "ALL" : "ANY"} ${getDestinations(destination)};`,
+  DROP_SUBSCRIPTION: ({ name, db, rp }) => `DROP SUBSCRIPTION "${name}" ON "${db}"."${rp}";`
+};
 
-  dropSubscription(opts) {
-    
+
+// exceptions
+
+class InfluxSubscriberError extends Error {
+  constructor(msg, err = null, ...params) {
+    super(...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor.name);
+    }
+
+    this.message = msg;
+    this.date = new Date();
+    this.originalError = err;
   }
 }
 
-module.exports = InfluxSubscriber;
+class ClientError extends InfluxSubscriberError {}
+
+class ServerError extends InfluxSubscriberError {}
+
+class RequestError extends InfluxSubscriberError {}
+
+
+// influx client declaration
+
+class InfluxClient {
+  constructor(opts = {}) {
+    this.proto = opts.proto || "http";
+    this.host = opts.url || "localhost";
+    this.port = opts.port || 8086;
+    this.url = getUrl(this.proto, this.host, this.port);
+    this.db = opts.db || "_default";
+    this.username = opts.user || null;
+    this.password = opts.password || null;
+    this._api = getApi(this.url);
+  }
+
+  async ping({ verbose=false } = {}) {
+    return await this.request({
+      url: "/ping",
+      method: "get",
+      params: {
+        verbose
+      },
+      query: "ping",
+      returnProp: "status"
+    });
+  }
+
+  async getDatabases({ db=this.db } = {}) {
+    const query = QUERY_LITERALS.GET_DATABASES();
+    const params = this.getParams({ db, q: query });
+
+    return await this.request({
+      method: "get",
+      params,
+      query
+    });
+  }
+
+  async createSubscription({ name, db=this.db, rp="autogen", all=true, destination } = {}) {
+    if (!name) {
+      throw new ClientError("Argument 'name' is required");
+    }
+
+    if (!destination || !(destination instanceof Array)) {
+      throw new ClientError("Argument 'destination' of type Array is required");
+    }
+
+    const query = QUERY_LITERALS.CREATE_SUBSCRIPTION({ name, db, rp, all, destination });
+    const params = this.getParams({ db, q: query });
+
+    return await this.request({
+      method: "post",
+      params,
+      query
+    });
+  }
+
+  async dropSubscription({ name, db=this.db, rp="autogen" } = {}) {
+    if (!name) {
+      throw new ClientError("Name argument is required");
+    }
+
+    const query =  QUERY_LITERALS.DROP_SUBSCRIPTION({ name, db, rp });
+    const params = this.getParams({ db, q: query });
+
+    return await this.request({
+      method: "post",
+      params,
+      query
+    });
+  }
+
+  async request({ url="/query", method, params, query, returnProp="data" }) {
+    try {
+      const response = await this._api({
+        url,
+        method,
+        params
+      });
+
+      return response[returnProp];
+    } catch (err) {
+      this.handleError(err, query);
+    }
+  }
+
+  handleError(error, query) {
+    if (error.response && error.response.status > 500) {
+      throw new ServerError(
+        `Got ${error.response.status} sending ${query}`,
+        error
+      );
+    }
+
+    if (error.response && error.response.status > 400) {
+      throw new ClientError(
+        `Got ${error.response.status} sending ${query}`,
+        error
+      );
+    }
+
+    if (error.request) {
+      throw new RequestError(
+        `Could not send ${query}`,
+        error
+      );
+    }
+
+    throw new Error("Unexpected error", error);
+  }
+
+  getParams({ db=this.db, q, u=this.username, p=this.password } = {}) {
+    return {
+      db,
+      q,
+      u,
+      p
+    };
+  }
+}
+
+module.exports = InfluxClient;
